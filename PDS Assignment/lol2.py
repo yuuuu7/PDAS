68782af81ac6d2a617a4cb62257cc330e47caa49
Yu <particlezgt@gmail.com>
2023-06-04 01:28:26 +0800

    Line Graph
    Line Graph completed for rental trend from 2021 to mid 2023

--- PDS Assignment/lol2.py
+++ PDS Assignment/lol2.py
@@ -1,23 +1,8 @@
 import numpy as np
 
-# File paths for the CSV files
-resale_1990_1999 = './resale-flat-price-CSVs/resale-flat-prices-based-on-approval-date-1990-1999.csv'
-resale_2000_2012 = './resale-flat-price-CSVs/resale-flat-prices-based-on-approval-date-2000-feb-2012.csv'
-resale_mar2012_dec2014 = './resale-flat-price-CSVs/resale-flat-prices-based-on-registration-date-from-mar-2012-to-dec-2014.csv'
-resale_jan2015_dec2016 = './resale-flat-price-CSVs/resale-flat-prices-based-on-registration-date-from-jan-2015-to-dec-2016.csv'
-resale_2017_onwards = './resale-flat-price-CSVs/resale-flat-prices-based-on-registration-date-from-jan-2017-onwards.csv'
-
-# Load the datasets from CSV files
-data1 = np.genfromtxt(resale_1990_1999, encoding=None, dtype=None, delimiter=',', names=True, usecols=("month", "town", "flat_type", "lease_commence_date", "resale_price"))
-data2 = np.genfromtxt(resale_2000_2012, encoding=None, dtype=None, delimiter=',', names=True, usecols=("month", "town", "flat_type", "lease_commence_date", "resale_price"))
-data3 = np.genfromtxt(resale_mar2012_dec2014, encoding=None, dtype=None, delimiter=',', names=True, usecols=("month", "town", "flat_type", "lease_commence_date", "resale_price"))
-data4 = np.genfromtxt(resale_jan2015_dec2016, encoding=None, dtype=None, delimiter=',', names=True, usecols=("month", "town", "flat_type", "lease_commence_date", "resale_price"))
-data5 = np.genfromtxt(resale_2017_onwards, encoding=None, dtype=None, delimiter=',', names=True, usecols=("month", "town", "flat_type", "lease_commence_date", "resale_price"))
-
-# Combine the datasets into one array
-combined_data = np.concatenate((data1, data2, data3, data4, data5))
-
-# Save the combined array to a new CSV file
-np.savetxt('combined_data.csv', combined_data, delimiter=',', fmt='%s')
-
-# The combined data is saved as 'combined_data.csv'
+data6_1flat = data6[data6['flat_type'] == keyword7]
+data6_2flat = data6[data6['flat_type'] == keyword8]
+data6_3flat = data6[data6['flat_type'] == keyword9]
+data6_4flat = data6[data6['flat_type'] == keyword10]
+data6_5flat = data6[data6['flat_type'] == keyword11]
+data6_Exec = data6[data6['flat_type'] == keyword12]
