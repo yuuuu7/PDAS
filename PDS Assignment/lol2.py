6251292a694f4f2b8cfa709fb498a27333997cb6
Yu <particlezgt@gmail.com>
2023-06-09 22:22:23 +0800

    Finished PDAS assignment
    Histogram plotted and Small scenario given
    
    All documentation (Markdowns, Overarching Question and Code Comments) have been included

--- PDS Assignment/lol2.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-data6_1flat = data6[data6['flat_type'] == keyword7]
-data6_2flat = data6[data6['flat_type'] == keyword8]
-data6_3flat = data6[data6['flat_type'] == keyword9]
-data6_4flat = data6[data6['flat_type'] == keyword10]
-data6_5flat = data6[data6['flat_type'] == keyword11]
-data6_Exec = data6[data6['flat_type'] == keyword12]
